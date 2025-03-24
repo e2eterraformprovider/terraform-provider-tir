cac3a12cbe847467326e80353fe542a4220fe77e
Jatin <jatin@Jatins-MacBook-Air.local>
2025-03-24 17:49:03 +0530

    docs updated

--- tir/Integration/resource_integration.go
+++ tir/Integration/resource_integration.go
@@ -1,5 +1,4 @@
 package integration
-
 import (
 	"context"
 	"log"
--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,7 @@ package tools
 // Format Terraform code for use in documentation.
 // If you do not have Terraform installed, you can remove the formatting command, but it is suggested
 // to ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ../e2e/
+//go:generate terraform fmt -recursive ../tir/
 
 // Generate documentation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name e2e
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name tir
