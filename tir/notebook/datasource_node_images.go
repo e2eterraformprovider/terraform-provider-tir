6859ba2211adb28cffa57bb28c9f96bf463dcf71
Jatin <jatin@Jatins-MacBook-Air.local>
2025-03-26 15:05:14 +0530

    some minor changes

--- tir/notebook/datasource_node_images.go
+++ tir/notebook/datasource_node_images.go
@@ -29,7 +29,7 @@ func DataSourceImages() *schema.Resource {
 				Computed: true,
 			},
 			"active_iam": {
-				Type:     schema.TypeInt,
+				Type:     schema.TypeString,
 				Required: true,
 			},
 			"category": {
